8639cef104a88d7454aacd2c2e763d13733e04f4
semihalev <semihalev@gmail.com>
2021-03-03 18:15:02 +0300

    update trade vue fix

--- web/static/js/trades.js
+++ web/static/js/trades.js
@@ -15,7 +15,7 @@ Vue.filter('filter_price', function(value) {
 });
 
 Vue.filter('round', function(value, decimalPlaces = 0) {
-  return value.toFixed(decimalPlaces);
+  return parseFloat(value).toFixed(decimalPlaces);
 });
 
 Vue.filter('date', function(value) {
