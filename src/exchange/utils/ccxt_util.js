41d2d392148e2dc3c533db7dfda532fb67b1f12a
semihalev <semihalev@gmail.com>
2021-04-29 13:03:36 +0300

    support partially filled order status fix

--- src/exchange/utils/ccxt_util.js
+++ src/exchange/utils/ccxt_util.js
@@ -10,7 +10,7 @@ module.exports = class CcxtUtil {
     let retry = false;
 
     let status;
-    const orderStatus = order.status.toLowerCase();
+    const orderStatus = order.status.toLowerCase().replace(/[\W_]+/g, '');
 
     if (['new', 'open', 'pendingnew', 'doneforday', 'stopped'].includes(orderStatus)) {
       status = ExchangeOrder.STATUS_OPEN;
