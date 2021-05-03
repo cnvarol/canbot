52d1c10cbf796ccf5e92153ccc09b82a3c7ff241
semihalev <semihalev@gmail.com>
2021-05-03 05:29:09 +0300

    small fix

--- web/static/js/trades.vue
+++ web/static/js/trades.vue
@@ -55,7 +55,7 @@
             <div class="card-body">
               <div class="row">
                 <div class="col-md-12">
-                  <h1 class="text-teal" style="font-size:50px">%{{ Math.round((balances.info.totalMaintMargin/balances.info.totalMarginBalance) * 100 * 100)/100 }}</h1>
+                  <h1 class="text-teal" style="font-size:50px">{{ Math.round((balances.info.totalMaintMargin/balances.info.totalMarginBalance) * 100 * 100)/100 }}%</h1>
                 </div>
               </div>
             </div>
