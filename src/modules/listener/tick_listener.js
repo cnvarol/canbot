47af8a8151cd0aad481aacb550f8a3e045fb636e
semihalev <semihalev@gmail.com>
2021-05-05 14:10:58 +0300

    telegram keyboard update #4

--- src/modules/listener/tick_listener.js
+++ src/modules/listener/tick_listener.js
@@ -123,7 +123,7 @@ module.exports = class TickListener {
       ]).reply();
 
       bot.start(ctx => ctx.reply(`Hey ${ctx.from.first_name} 😜 I'm ready for your commands.`, keyboard));
-      bot.stop(ctx => {
+      bot.command('stop', ctx => {
         ctx.reply(`${ctx.from.first_name}, you need start me again on terminal.\nStopping..`);
         process.exit(0);
       });
