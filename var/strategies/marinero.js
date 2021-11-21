e420f0b26d21b10408f766f2c90be6d0ae6fa1af
semihalev <semihalev@gmail.com>
2021-11-21 15:05:54 +0300

    marinero update #3

--- var/strategies/marinero.js
+++ var/strategies/marinero.js
@@ -28,7 +28,7 @@ module.exports = class {
     const fisher_rsi = (Math.exp(2 * rsiP) - 1) / (Math.exp(2 * rsiP) + 1);
 
     const long = fisher_rsi <= -0.9;
-    const short = fisher_rsi >= 0.99 && sma50 / sma200 - 1 > 0.5 && mfi > 80;
+    const short = fisher_rsi >= 0.99 && sma50 / sma200 - 1 > 0.25 && mfi > 85;
 
     const lastSignal = indicatorPeriod.getLastSignal();
 
